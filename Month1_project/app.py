bcb59803819e8a1957d297dc02c2093dfd89917e
Bandi120424 <hny0309@gmail.com>
2022-10-20 17:39:31 +0900

    Update app.py

--- Month1_project/app.py
+++ Month1_project/app.py
@@ -1,13 +1,17 @@
+#requirements.txt 필요
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+import plotly.express as px 
 import plotly.graph_objects as go
 
+
+st.set_page_config(layout= "wide")
 
 insurance_data = pd.read_csv("Month1_project/insurance.csv")
 
 st.title("EDA on Medical Cost in the beneficiary's residential area in the US")
+st.write("made by bandi12424@naver.com")
 
 ### 각 지역별로 데이터 나누기 
 southwest_data = insurance_data.query("region == 'southwest'")
@@ -15,13 +19,17 @@ northwest_data = insurance_data.query("region == 'northwest'")
 southeast_data = insurance_data.query("region == 'southeast'")
 northeast_data = insurance_data.query("region == 'northeast'")
 
-
-###나이별, 성별 요금 
-st.header("Charges: age & sex in each region ")
-st.subheader("Southwest Region")
+#########지역별 age & sex 기준 plot#################
 #scatter plot
 SW_charge_age_fig_MF = px.scatter(southwest_data, x = 'age', y='charges', color = 'sex')
-
+SW_charge_age_fig_MF.update_layout(
+    title={
+        'text': "Southwest",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'}
+)
 SW_charge_age_fig_MF.update_xaxes(autorange=True)
 SW_charge_age_fig_MF.update_yaxes(autorange=True, tickprefix="$")
 
@@ -29,7 +37,7 @@ SW_charge_age_fig_MF.update_yaxes(autorange=True, tickprefix="$")
 SW_charge_age_bar_fig_MF = px.bar(southwest_data, x = 'age', y='charges', facet_col = 'sex')
 SW_charge_age_bar_fig_MF.update_layout(
     title={
-        'text': "Southwest region charges per age",
+        'text': "Southwest",
         'y':0.95,
         'x':0.5,
         'xanchor': 'center',
@@ -38,15 +46,16 @@ SW_charge_age_bar_fig_MF.update_layout(
 SW_charge_age_fig_MF.update_xaxes(autorange=True)
 SW_charge_age_fig_MF.update_yaxes(autorange=True)
 
-#display
-st.plotly_chart(SW_charge_age_bar_fig_MF)
-st.plotly_chart(SW_charge_age_fig_MF)
-
-
-st.subheader("Southeast Region")
 #scatter plot
 SE_charge_age_fig_MF = px.scatter(southeast_data, x = 'age', y='charges', color = 'sex')
-
+SE_charge_age_fig_MF.update_layout(
+    title={
+        'text': "Southeast region charges per age",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'}
+)
 SE_charge_age_fig_MF.update_xaxes(autorange=True)
 SE_charge_age_fig_MF.update_yaxes(autorange=True, tickprefix="$")
 
@@ -63,13 +72,16 @@ SE_charge_age_bar_fig_MF.update_layout(
 SE_charge_age_fig_MF.update_xaxes(autorange=True)
 SE_charge_age_fig_MF.update_yaxes(autorange=True)
 
-st.plotly_chart(SE_charge_age_bar_fig_MF)
-st.plotly_chart(SE_charge_age_fig_MF)
-
-st.subheader("Northwest Region")
 #scatter plot
 NW_charge_age_fig_MF = px.scatter(northwest_data, x = 'age', y='charges', color = 'sex')
-
+NW_charge_age_fig_MF.update_layout(
+    title={
+        'text': "Northwest region charges per age",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'}
+)
 NW_charge_age_fig_MF.update_xaxes(autorange=True)
 NW_charge_age_fig_MF.update_yaxes(autorange=True, tickprefix="$")
 
@@ -86,13 +98,16 @@ NW_charge_age_bar_fig_MF.update_layout(
 NW_charge_age_fig_MF.update_xaxes(autorange=True)
 NW_charge_age_fig_MF.update_yaxes(autorange=True)
 
-st.plotly_chart(NW_charge_age_bar_fig_MF)
-st.plotly_chart(NW_charge_age_fig_MF)
-
-st.subheader("Northeast Region")
 #scatter plot
 NE_charge_age_fig_MF = px.scatter(northeast_data, x = 'age', y='charges', color = 'sex')
-
+NE_charge_age_fig_MF.update_layout(
+    title={
+        'text': "Northwest region charges per age",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'}
+)
 NE_charge_age_fig_MF.update_xaxes(autorange=True)
 NE_charge_age_fig_MF.update_yaxes(autorange=True, tickprefix="$")
 
@@ -109,17 +124,17 @@ NE_charge_age_bar_fig_MF.update_layout(
 NE_charge_age_fig_MF.update_xaxes(autorange=True)
 NE_charge_age_fig_MF.update_yaxes(autorange=True)
 
-st.plotly_chart(NE_charge_age_bar_fig_MF)
-st.plotly_chart(NE_charge_age_fig_MF)
-
-
-###흡연여부, 성별 요금 
-st.header("Charges: smoker & sex in each region ")
-
-st.subheader("Southwest Region")
+############## 각 지역별 흡연여부, 성별 기준 plot ###########################
 #scatter plot 
 SW_charge_age_fig_SM = px.scatter(southwest_data, x = 'age', y='charges', color = 'smoker')
-
+SW_charge_age_fig_SM.update_layout(
+    title={
+        'text': "Southwest",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'}
+)
 SW_charge_age_fig_SM.update_xaxes(autorange=True)
 SW_charge_age_fig_SM.update_yaxes(autorange=True, tickprefix="$")
 
@@ -127,7 +142,7 @@ SW_charge_age_fig_SM.update_yaxes(autorange=True, tickprefix="$")
 SW_charge_age_bar_fig_SM = px.bar(southwest_data, x = 'age', y='charges', facet_col = 'smoker')
 SW_charge_age_bar_fig_SM.update_layout(
     title={
-        'text': "Southwest region charges: age & smoking",
+        'text': "Southwest",
         'y':0.95,
         'x':0.5,
         'xanchor': 'center',
@@ -136,7 +151,88 @@ SW_charge_age_bar_fig_SM.update_layout(
 SW_charge_age_fig_SM.update_xaxes(autorange=True)
 SW_charge_age_fig_SM.update_yaxes(autorange=True)
 
-# 나이-흡연여부-성별 순으로 sunburst chart를 그려봅시다.
+
+#scatter plot 
+SE_charge_age_fig_SM = px.scatter(southeast_data, x = 'age', y='charges', color = 'smoker')
+SE_charge_age_fig_SM.update_layout(
+    title={
+        'text': "Southeast",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'}
+)
+SE_charge_age_fig_SM.update_xaxes(autorange=True)
+SE_charge_age_fig_SM.update_yaxes(autorange=True, tickprefix="$")
+
+#bar chart 
+SE_charge_age_bar_fig_SM = px.bar(southeast_data, x = 'age', y='charges', facet_col = 'smoker')
+SE_charge_age_bar_fig_SM.update_layout(
+    title={
+        'text': "Southeast",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'}
+)
+SE_charge_age_fig_SM.update_xaxes(autorange=True)
+SE_charge_age_fig_SM.update_yaxes(autorange=True)
+
+#scatter plot 
+NW_charge_age_fig_SM = px.scatter(northwest_data, x = 'age', y='charges', color = 'smoker')
+NW_charge_age_fig_SM.update_layout(
+    title={
+        'text': "Northwest",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'}
+)
+NW_charge_age_fig_SM.update_xaxes(autorange=True)
+NW_charge_age_fig_SM.update_yaxes(autorange=True, tickprefix="$")
+
+#bar chart 
+NW_charge_age_bar_fig_SM = px.bar(northwest_data, x = 'age', y='charges', facet_col = 'smoker')
+NW_charge_age_bar_fig_SM.update_layout(
+    title={
+        'text': "Northwest",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'}
+)
+NW_charge_age_fig_SM.update_xaxes(autorange=True)
+NW_charge_age_fig_SM.update_yaxes(autorange=True)
+
+
+#scatter plot 
+NE_charge_age_fig_SM = px.scatter(northeast_data, x = 'age', y='charges', color = 'smoker')
+NE_charge_age_fig_SM.update_layout(
+    title={
+        'text': "Northeast",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'}
+)
+NE_charge_age_fig_SM.update_xaxes(autorange=True)
+NE_charge_age_fig_SM.update_yaxes(autorange=True, tickprefix="$")
+
+#bar chart 
+NE_charge_age_bar_fig_SM = px.bar(northeast_data, x = 'age', y='charges', facet_col = 'smoker')
+NE_charge_age_bar_fig_SM.update_layout(
+    title={
+        'text': "Northeast",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'}
+)
+NE_charge_age_fig_SM.update_xaxes(autorange=True)
+NE_charge_age_fig_SM.update_yaxes(autorange=True)
+
+
+############ 지역별 나이-흡연여부-성별 순으로 sunburst chart##################
 SW_sunburst_fig = px.sunburst(southwest_data, path=['smoker', 'sex'], values = 'charges', title = 'charges per smoker and sex')
 colors=['#EF553B',
          '#636EFA']
@@ -154,6 +250,12 @@ for i, m in enumerate(criterion):
                                         yref="paper"))
 
 SW_sunburst_fig.update_layout(
+     title={
+        'text': "Southwest",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)'
 )
@@ -161,32 +263,7 @@ SW_sunburst_fig.update_layout(
 SW_sunburst_fig.update_xaxes(tickfont=dict(color='rgba(0,0,0,0)'))
 SW_sunburst_fig.update_yaxes(tickfont=dict(color='rgba(0,0,0,0)'))
 
-st.plotly_chart(SW_sunburst_fig)
-st.plotly_chart(SW_charge_age_fig_SM)
-st.plotly_chart(SW_charge_age_bar_fig_SM)
 
-
-st.subheader("Southeast Region")
-#scatter plot 
-SE_charge_age_fig_SM = px.scatter(southeast_data, x = 'age', y='charges', color = 'smoker')
-
-SE_charge_age_fig_SM.update_xaxes(autorange=True)
-SE_charge_age_fig_SM.update_yaxes(autorange=True, tickprefix="$")
-
-#bar chart 
-SE_charge_age_bar_fig_SM = px.bar(southeast_data, x = 'age', y='charges', facet_col = 'smoker')
-SE_charge_age_bar_fig_SM.update_layout(
-    title={
-        'text': "Southeast region charges: age & smoking",
-        'y':0.95,
-        'x':0.5,
-        'xanchor': 'center',
-        'yanchor': 'top'}
-)
-SE_charge_age_fig_SM.update_xaxes(autorange=True)
-SE_charge_age_fig_SM.update_yaxes(autorange=True)
-
-# 나이-흡연여부-성별 순으로 sunburst chart를 그려봅시다.
 SE_sunburst_fig = px.sunburst(southeast_data, path=['smoker', 'sex'], values = 'charges', title = 'charges per smoker and sex')
 colors=['#EF553B',
          '#636EFA']
@@ -204,6 +281,12 @@ for i, m in enumerate(criterion):
                                         yref="paper"))
 
 SE_sunburst_fig.update_layout(
+    title={
+        'text': "Southeast",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)'
 )
@@ -211,31 +294,7 @@ SE_sunburst_fig.update_layout(
 SE_sunburst_fig.update_xaxes(tickfont=dict(color='rgba(0,0,0,0)'))
 SE_sunburst_fig.update_yaxes(tickfont=dict(color='rgba(0,0,0,0)'))
 
-st.plotly_chart(SE_sunburst_fig)
-st.plotly_chart(SE_charge_age_fig_SM)
-st.plotly_chart(SE_charge_age_bar_fig_SM)
 
-st.subheader("Northwest Region")
-#scatter plot 
-NW_charge_age_fig_SM = px.scatter(northwest_data, x = 'age', y='charges', color = 'smoker')
-
-NW_charge_age_fig_SM.update_xaxes(autorange=True)
-NW_charge_age_fig_SM.update_yaxes(autorange=True, tickprefix="$")
-
-#bar chart 
-NW_charge_age_bar_fig_SM = px.bar(northwest_data, x = 'age', y='charges', facet_col = 'smoker')
-NW_charge_age_bar_fig_SM.update_layout(
-    title={
-        'text': "Northwest region charges: age & smoking",
-        'y':0.95,
-        'x':0.5,
-        'xanchor': 'center',
-        'yanchor': 'top'}
-)
-NW_charge_age_fig_SM.update_xaxes(autorange=True)
-NW_charge_age_fig_SM.update_yaxes(autorange=True)
-
-# 나이-흡연여부-성별 순으로 sunburst chart를 그려봅시다.
 NW_sunburst_fig = px.sunburst(northwest_data, path=['smoker', 'sex'], values = 'charges', title = 'charges per smoker and sex')
 colors=['#EF553B',
          '#636EFA']
@@ -253,39 +312,21 @@ for i, m in enumerate(criterion):
                                         yref="paper"))
 
 NW_sunburst_fig.update_layout(
+     title={
+        'text': "Northwest",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)'
 )
 
+
 NW_sunburst_fig.update_xaxes(tickfont=dict(color='rgba(0,0,0,0)'))
 NW_sunburst_fig.update_yaxes(tickfont=dict(color='rgba(0,0,0,0)'))
 
-st.plotly_chart(NW_sunburst_fig)
-st.plotly_chart(NW_charge_age_fig_SM)
-st.plotly_chart(NW_charge_age_bar_fig_SM)
 
-
-st.subheader("Northeast Region")
-#scatter plot 
-NE_charge_age_fig_SM = px.scatter(northeast_data, x = 'age', y='charges', color = 'smoker')
-
-NE_charge_age_fig_SM.update_xaxes(autorange=True)
-NE_charge_age_fig_SM.update_yaxes(autorange=True, tickprefix="$")
-
-#bar chart 
-NE_charge_age_bar_fig_SM = px.bar(northeast_data, x = 'age', y='charges', facet_col = 'smoker')
-NE_charge_age_bar_fig_SM.update_layout(
-    title={
-        'text': "Northeast region charges: age & smoking",
-        'y':0.95,
-        'x':0.5,
-        'xanchor': 'center',
-        'yanchor': 'top'}
-)
-NE_charge_age_fig_SM.update_xaxes(autorange=True)
-NE_charge_age_fig_SM.update_yaxes(autorange=True)
-
-# 나이-흡연여부-성별 순으로 sunburst chart를 그려봅시다.
 NE_sunburst_fig = px.sunburst(northeast_data, path=['smoker', 'sex'], values = 'charges', title = 'charges per smoker and sex')
 colors=['#EF553B',
          '#636EFA']
@@ -303,6 +344,12 @@ for i, m in enumerate(criterion):
                                         yref="paper"))
 
 NE_sunburst_fig.update_layout(
+    title={
+        'text': "Northeast",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)'
 )
@@ -310,11 +357,79 @@ NE_sunburst_fig.update_layout(
 NE_sunburst_fig.update_xaxes(tickfont=dict(color='rgba(0,0,0,0)'))
 NE_sunburst_fig.update_yaxes(tickfont=dict(color='rgba(0,0,0,0)'))
 
-st.plotly_chart(NE_sunburst_fig)
-st.plotly_chart(NE_charge_age_fig_SM)
-st.plotly_chart(NE_charge_age_bar_fig_SM)
+#############DISPLAY#################
 
-#동적기능을 만들어보자 
+
+tab1, tab2, tab3 = st.tabs(["age & sex", "age & smoking", "smoking ratio"] ) #max tab 갯수 = 3
+
+with tab1: ###나이별, 성별 요금 
+    st.header("Charges: age & sex in each region ")
+    #display
+    d_col5, d_col6 = st.columns(2)
+    with d_col5:
+        st.plotly_chart(SW_charge_age_fig_MF)
+    with d_col6:
+        st.plotly_chart(SE_charge_age_fig_MF)
+
+    d_col7, d_col8 = st.columns(2)
+    with d_col7:
+        st.plotly_chart(NW_charge_age_fig_MF)
+    with d_col8:
+        st.plotly_chart(NE_charge_age_fig_MF)
+
+    d_col1, d_col2 = st.columns(2)
+    with d_col1:
+        st.plotly_chart(SW_charge_age_bar_fig_MF)
+    with d_col2:
+        st.plotly_chart(SE_charge_age_bar_fig_MF)
+        
+    d_col3, d_col4 = st.columns(2)
+    with d_col3:
+        st.plotly_chart(NW_charge_age_bar_fig_MF)
+    with d_col4:
+        st.plotly_chart(NE_charge_age_bar_fig_MF)
+
+with tab2: 
+    st.header("Charges: age & smoking in each region ")
+    #display
+    d_col9, d_col10 = st.columns(2)
+    with d_col9:
+        st.plotly_chart(SW_charge_age_fig_SM)
+    with d_col10:
+        st.plotly_chart(SE_charge_age_fig_SM)    
+
+    d_col11, d_col12 = st.columns(2)
+    with d_col11:
+        st.plotly_chart(NW_charge_age_fig_SM)
+    with d_col12:
+        st.plotly_chart(NE_charge_age_fig_SM)
+
+    d_col13, d_col14 = st.columns(2)
+    with d_col13:
+        st.plotly_chart(SW_charge_age_bar_fig_SM)
+    with d_col14:
+        st.plotly_chart(SE_charge_age_bar_fig_SM)
+
+    d_col15, d_col16 = st.columns(2)
+    with d_col15:
+        st.plotly_chart(NW_charge_age_bar_fig_SM)
+    with d_col16:
+        st.plotly_chart(NE_charge_age_bar_fig_SM)
+    
+with tab3: #smoking ratio
+    st.header("smoking ratio in each region ")
+    d_col17, d_col18 = st.columns(2)
+    with d_col17:
+        st.plotly_chart(SW_sunburst_fig)
+    with d_col18:
+        st.plotly_chart(SE_sunburst_fig)
+
+    d_col19, d_col20 = st.columns(2)
+    with d_col19:
+        st.plotly_chart(NW_sunburst_fig)
+    with d_col20:
+        st.plotly_chart(NE_sunburst_fig)
+
 #지역과 나이, 성별, 흡연여부를 입력했을 때, charges의 평균을 구하도록 해보자 
 st.header("Averages on charges")
 
@@ -332,7 +447,7 @@ if st.button("submit"): #버튼이 눌리면
 
     result = insurance_data[op_region & op_age & op_sex & op_smoking]
     avg = round(result['charges'].mean(),2)
-    
+        
     if len(result) == 0: #해당하는 데이터가 없음
         st.write("There is no such data. Please choose another value")
     else:
@@ -341,8 +456,8 @@ if st.button("submit"): #버튼이 눌리면
         col2.metric("Region", value = region)
         col3.metric("Sex", value = sex)
         col4.metric("Smoker", value = smoking)
-            
-        st.subheader(f"Average charge {avg} dollar")
+                
+        st.subheader(f"Average charge: {avg} dollar")
         st.write("selected dataset")
         st.dataframe(result)
 else:
