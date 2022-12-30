f666bda572ebec5531aca81b5a85119fdc634526
Bandi120424 <hny0309@gmail.com>
2022-12-30 10:08:23 +0900

    Update bmi.py

--- Month1_project/bmi.py
+++ Month1_project/bmi.py
@@ -15,10 +15,14 @@ northeast_data = df.df_northeast_data()
 insurance_data = df.df_insure_data()
 
 #저제중, 정상체중, 과제충, 비만 기분으로 데이터 나눔 
+criterion = pd.DataFrame ({'Low': ['0 - 19'], 'Normal':['20 - 24'], 'Overweight':['25 - 29'], 'Obesity':['over 30']})
+criterion.style.hide_index()
+
 bmi_low = (0<= insurance_data['bmi']) & (insurance_data['bmi'] < 20)
 bmi_normal = (20<= insurance_data['bmi']) & (insurance_data['bmi'] < 25)
 bmi_over = (25 <= insurance_data['bmi']) & (insurance_data['bmi'] < 30)
 bmi_obe = (30<= insurance_data['bmi'])
+
 
 bmi_low_df = insurance_data[bmi_low]
 bmi_nor_df = insurance_data[bmi_normal]
@@ -28,14 +32,17 @@ bmi_obe_df = insurance_data[bmi_obe]
 #group별 BMI 평균 
 val_y = [bmi_low_df['charges'].mean(),  bmi_nor_df['charges'].mean(), bmi_over_df['charges'].mean(), bmi_obe_df['charges'].mean()]
 val_x = ['0 - 19', '20 - 24', '25 - 29', 'over 30']
-bmi_group_avg_bar_fig = px.bar(x = val_x, y =val_y)
+bmi_group_avg_bar_fig = px.bar(x = val_x, y =val_y, color = val_x)
 bmi_group_avg_bar_fig.update_layout(
     title={
-        'text': 'x: BMI, y: charges',
         'y':0.825,
         'x':0.15,
         'xanchor': 'left',
-        'yanchor': 'top'}
+        'yanchor': 'top'},
+    xaxis_title = 'BMI',
+    yaxis_title = 'average charges',
+    showlegend=False,
+    autosize = True
 )
 
 #각 group에서 BMI에 따른 평균 charge 
