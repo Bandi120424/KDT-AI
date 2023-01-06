1f9b364776c9a60aa091007b479a9e1603d71064
Bandi120424 <hny0309@gmail.com>
2023-01-06 22:52:07 +0900

    Update home.py

--- EDA_medical_cost/pages/home.py
+++ EDA_medical_cost/pages/home.py
@@ -18,22 +18,5 @@ def home():
                 - region: the beneficiary residential area in the US, northeast, southeast, southwest, northwest
                 - charges: individual medical costs billed by health insurance
                 """ )
-
-def add_bg_from_local(image_file):
-        with open(image_file, "rb") as image_file:
-            encoded_string = base64.b64encode(image_file.read())
-        st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
-            opacity:0.2
-            background-size: cover;    
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-        )
-    
     
     
